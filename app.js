e77414e4555cb18b021ca3c7180b445097e2c149
OpenTech-Consult <opentech.consult@gmail.com>
2021-12-29 15:26:40 +0000

    Routing Requests
    modified the web server created to render different url according to the route entered in the browser
    
    closes #5

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var http = require('http');
 
 const server = http.createServer((request, response) => {
-  console.log(request.url, request.method, request.headers);
-  response.setHeader('Content-Type', 'text/html');
-  response.write('<html>');
-  response.write('<head><title>My First Page</title></head>');
-  response.write('<body><h1>Hello from my Node.js server</h1></body>');
-  response.write('</html>');
-  response.end();
+    const url = request.url;
+    response.setHeader('Content-Type', 'text/html');
+    if (url === '/') {
+        response.write('<html>');
+        response.write('<head><title>Enter Message</title></head>');
+        response.write('<body><form action="/message" method="POST"><input type="text" name="message"><input type="submit" value="Enter Message"></form></body>');
+        response.write('</html>');
+        return response.end();
+    }
+    response.write('<html>');
+    response.write('<head><title>My First Page</title></head>');
+    response.write('<body><h1>Hello from my Node.js server</h1></body>');
+    response.write('</html>');
+    response.end();
+
 });
 
 server.listen(3000);
