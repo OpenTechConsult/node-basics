e25f37f85a767f8d86304f2f9dd29555adad3f92
OpenTech-Consult <opentech.consult@gmail.com>
2021-12-30 07:43:58 +0000

    Enhance the request.on('end')
    Turned the writeFileSync() into writeFile() in order to write to the file asynchronously
    Put the redirection logic into the writeFile() handler method.
    
    Closes #9

--- app.js
+++ app.js
@@ -25,13 +25,14 @@ const server = http.createServer((request, response) => {
             const parsedBody = Buffer.concat(body).toString();
             console.log(parsedBody)
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-            response.statusCode = 302;
-            response.setHeader('Location', '/');
-            response.end();
-        });
-        
+            fs.writeFile('message.txt', message, (err) => {
+                response.statusCode = 302;
+                response.setHeader('Location', '/');
+                response.end();
+            });
+        });    
     }
+    
     response.write('<html>');
     response.write('<head><title>My First Page</title></head>');
     response.write('<body><h1>Hello from my Node.js server</h1></body>');
