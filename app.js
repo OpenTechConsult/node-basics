6d3518db799e8aa6601cdc456c8c1f83da6810d9
OpenTech-Consult <opentech.consult@gmail.com>
2022-01-04 11:58:48 +0000

    Placed semi colon where the are forgotten.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.use((req, res, next) => {
     console.log('In the middleware');
