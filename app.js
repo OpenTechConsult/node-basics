ac65f30ed288609f831a6ad4b547a209a5e0b71f
OpenTech-Consult <opentech.consult@gmail.com>
2022-01-04 14:16:57 +0000

    handling different route
    fix the typo we have in the code on line 15. add the route "/" in the use() middleware function

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/add-product', (req, res, next) => {
     res.send('<h1>The "Add Product" page</h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In another middleware');
     res.send('<h1>Hello from Express!</h1>');
 });
