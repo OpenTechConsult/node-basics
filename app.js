042490729df28ddd234f9d618dc46619c3289558
OpenTech-Consult <opentech.consult@gmail.com>
2022-01-04 13:40:57 +0000

    Express JS behind the curtains and Handling different route
    - saw the use() middleware function is written in express
    - use express app function to boot the server up and listen to the port 3000
    - see use() middleware function is used for handling different routes.
    
    closes #20

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
+app.use('/', (req, res, next) => {
+    console.log('This always runs');
     next();
+})
+
+app.use('/add-product', (req, res, next) => {
+    console.log('In the middleware');
+    res.send('<h1>The "Add Product" page</h1>');
 });
 
 app.use((req, res, next) => {
     console.log('In another middleware');
-    res.send('<h1>HELLO FROM EXPRESS JS</h1>');
+    res.send('<h1>Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
+app.listen(3000);
 
-server.listen(3000);
