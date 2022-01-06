d71fbcd5d7ec45208f51b46f99bbfd6f36077c5f
OpenTech-Consult <opentech.consult@gmail.com>
2022-01-06 11:51:23 +0000

    In Filtering Paths
    We want all admin related routes to start with "/admin".
    We using path filtering to achieve that in app.js
    
    Closes #30

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,10 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="post"><label for="product">Enter a product</label><input type="text" name="product" id="product"><button type="submit">Add a product</button></form>');
+    res.send('<form action="/admin/add-product" method="post"><label for="product">Enter a product</label><input type="text" name="product" id="product"><button type="submit">Add a product</button></form>');
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
