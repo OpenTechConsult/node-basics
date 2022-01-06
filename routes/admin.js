0d4632192e93b1213d58236c0589b692ce8d39c4
OpenTech-Consult <opentech.consult@gmail.com>
2022-01-06 19:44:35 +0000

    refactor: change the routes handler middleware to allow middleware functions to send files
    - make GET /add-product route handler middleware function to send add-product.html HTML file
    - make GET / route handler middleware function to send shop.html HTML file
    - make created the 404.html file inside the views folder
    - makes the default middleware function that handle all unmatched route to send a 404 status code and send the 404.html file created.
    
    closes #34

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 const express = require('express')
 
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="post"><label for="product">Enter a product</label><input type="text" name="product" id="product"><button type="submit">Add a product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
