ed0bd25c0143757f11c3a748b3faf328edca6f55
OpenTech-Consult <opentech.consult@gmail.com>
2022-01-21 19:16:12 +0000

    Adding Product Model
    Integrate the M "Model" part of the MVC pattern we are implementing.
    
    created the Product model
    used the Product model in the product controller file to save a Product in the postAddProduct controller fonction and to get product
    
    Storing model data in files and fetching from file via the model.
    refactoring file storage
    
    closes #65

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/Product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -12,18 +12,21 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({product: req.body.product});
+    const product = new Product(req.body.product);
+    product.save();
     res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-    res.render("shop", {
-      prods: products,
-      pageTitle: "Shop",
-      path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true,
-    });
+    Product.fetchAll((products) => {  
+      res.render("shop", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/",
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true,
+      });
+    });  
 };
 
