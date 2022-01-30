649d4152adc02e98a086011a4f8ae2e6282d1b0c
OpenTech-Consult <opentech.consult@gmail.com>
2022-01-30 19:26:09 +0000

    Registering new routes
    In order to display the new routes we created we registered new routes
    and in we made some changes to existing routes as well linking the new routes to their respectives controllers
    
    in the routes/admin.js file we introduced the following changes
    - added the GET /admin/add-product route with the route handler being getAddProduct from the controllers/admin
    - added the GET /admin/products route with the route handler being getProducts controller from the controllers/admin
    - changed the POST /admin/add-product route with the route being postAddProduct from the controllers/admin
    - refactored the productController constant to adminController.

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,23 +9,20 @@ exports.getAddProduct = (req, res, next) => {
     activeAddProduct: true,
   });
 };
-
+  
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.product);
-    product.save();
-    res.redirect('/');
+  const product = new Product(req.body.product);
+  product.save();
+  res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {  
-      res.render("shop/product-list", {
-        prods: products,
-        pageTitle: "Shop",
-        path: "/",
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true,
-      });
-    });  
+  Product.fetchAll((products) => {  
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products"
+    });
+  });
 };
-
+  
